5a6de1da37137bfb919e9d3f8a94bfb98ddc72b6
sArwocha <121029273+sArwocha@users.noreply.github.com>
2023-08-06 22:13:16 +0545

    Create Vowel.cs

---
+++ Vowel.cs
@@ -0,0 +1,20 @@
+//# VowelFind
+//find vowel from String
+using system
+namespace myprogram{
+class vwl{
+public static void Main(string[] args){
+Console.WriteLine("Enter The string");
+string str=Console.Readline();
+string new="";
+str=str.ToLower();
+for(int i=0;i<str.length;i++){
+if (str[i]='a'|| str[i]='e'|| str[i]='i'|| str[i]='o'|| str[i]='u'||){
+new+=str[i];
+}
+}
+Console.WriteLine("The vowels are ::"+new);
+Console.ReadLine();
+}
+}
+}
